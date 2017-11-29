bbc5ac9bde57cfbfa16d44036c732c66a23f6356
Charlotte Jewer <charlotte.jewer@hotmail.co.uk>
2017-11-29 15:01:26 +0000

    even more refactoring

--- leap/leap.py
+++ leap/leap.py
@@ -2,9 +2,7 @@ def is_leap_year(year):
     if year % 4 ==0:
         if year % 100 != 0:
             return True
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
+        if year % 100 == 0 and year % 400 == 0:
+            return True
+        return False
     return False
